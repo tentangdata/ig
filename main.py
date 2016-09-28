4ef63ca7fb8b1611c056cd10234e63e31be59a0f
Ali Akbar <mail.septiandri@gmail.com>
2016-09-28 21:51:56 +0700

    Changed to psycopg2; Reading directly from text files

--- main.py
+++ main.py
@@ -1,52 +1,76 @@
 #!/usr/bin/python
 from datetime import datetime
 from flask import Flask, flash, render_template, request, redirect
-import MySQLdb as mdb
 import json
+import psycopg2
+import os
+import random
 
 app = Flask(__name__)
 app.secret_key = 'instagram-screen-scraper'
 
+file_in_dir = '/home/aliakbars/Dropbox/Spam/comments_new/'
+file_out_dir = '/home/aliakbars/Dropbox/Spam/label/'
+
 def getUnannotatedComment():
     try:
-        con = mdb.connect('localhost', 'root', 'root', 'instagram')
-        con.set_character_set('utf8')
+        con = psycopg2.connect(host=os.ENVIRON['PSQL_HOST'], user=os.ENVIRON['PSQL_USER'], password=os.ENVIRON['PSQL_PASS'], dbname=os.ENVIRON['PSQL_INSTA'])
         cur = con.cursor()
-        query = "SELECT * from comments where label is not null"
+        query = "SELECT * from comments where label is not null offset random() * (select count(*) from comments) limit 1"
         cur.execute(query)
         return cur.fetchone()
-    except mdb.Error, e:
-        print "Error %d: %s" % (e.args[0],e.args[1])
+    except psycopg2.Error as e:
+        print e.pgerror
     finally:
         con.close()
 
-def insertComment(data):
+def insertComment(data, filename):
     try:
-        con = mdb.connect('localhost', 'root', 'root', 'instagram')
-        con.set_character_set('utf8')
+        con = psycopg2.connect(host=os.environ['PSQL_HOST'], user=os.environ['PSQL_USER'], password=os.environ['PSQL_PASS'], dbname=os.environ['PSQL_INSTA'])
         cur = con.cursor()
-        cur.execute("INSERT INTO comments(`id`,`username`,`time`,`text`) values(%s,%s,%s,%s)", (data['id'], data['username'], datetime.fromtimestamp(data['time'] / 1e3), data['text']))
+        cur.execute("INSERT INTO comments(id,username,time,text,filename) values(%s,%s,%s,%s,%s)", (data['id'], data['username'], datetime.fromtimestamp(data['time'] / 1e3), data['text'], filename,))
         con.commit()
-    except mdb.Error, e:
-        print "Error %d: %s" % (e.args[0],e.args[1])
+    except psycopg2.Error as e:
+        print e.pgerror
     finally:
         con.close()
+
+def updateLabel(comment_id, label):
+    try:
+        con = psycopg2.connect(host=os.environ['PSQL_HOST'], user=os.environ['PSQL_USER'], password=os.environ['PSQL_PASS'], dbname=os.environ['PSQL_INSTA'])
+        cur = con.cursor()
+        cur.execute("UPDATE comments SET label=%s WHERE id=%s", (label, comment_id,))
+        con.commit()
+    except psycopg2.Error as e:
+        print e.pgerror
+    finally:
+        con.close()
+
+def getComments():
+    file_in = os.listdir(file_in_dir)
+    file_out = os.listdir(file_out_dir)
+    f = random.sample(set(file_in) - set(file_out), 1)[0]
+    return json.loads(open(file_in_dir + f).read())[:50], f
+
+def writeFile(data, file_out):
+    with open(file_out_dir + file_out, 'w') as f:
+        f.write(data)
+        f.flush()
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        if not request.form['id'] or not request.form['spam']:
-            flash('Make sure that every field has been filled!', 'error')
-        else:
-            flash('Label saved', 'info')
-            return redirect('/')
-    data = getUnannotatedComment()
-    # data = {'text': 'Oh ya ya ya', 'id': '123456'}
-    return render_template('index.html', data=data)
+        flash('Label saved', 'info')
+        writeFile(json.dumps(dict(zip(request.form.getlist('id[]'), request.form.getlist('label[]')))), request.form.get('filename'))
+        return redirect('/')
+    data, f = getComments()
+    return render_template('index.html', data=data, filename=f)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
-    # filename = 'inijedar_BKnxd0rhHJz.json'
-    # f = open('/home/aliakbars/Dropbox/Spam/comments/{}'.format(filename))
-    # data = json.loads(f.read().strip())
-    # insertComment(data[0])
+    # filedir = '/home/aliakbars/Dropbox/Spam/comments_new/'
+    # for filename in os.listdir(filedir):
+    #     print filename
+    #     comments = json.loads(open(filedir + filename).read())
+    #     for comment in comments:
+    #         insertComment(comment, filename)
